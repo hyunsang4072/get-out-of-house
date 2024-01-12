915bed0ee5ff4034f4f5ed01df890f24808fb701
hyunsang4072 <hyunsang4072@gmail.com>
2024-01-11 17:20:29 -0800

    finish travel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 function App() {
   return (
@@ -19,20 +25,61 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    console.log(newItem);
+
+    // reset form after submission
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className='add-form'>
+    <form className='add-form' onClick={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type='text' placeholder='Item...' value={description} onChange={(e) => setDescription(e.target.value)} />
+      <button>add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className='list'>LIST</div>;
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map(item => (<Item item={item} key={item.id} />))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>{item.quantity} {item.description}</span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className='stats'>
       <em>You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
